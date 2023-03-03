ece669aff535bebde3c914d7c84e23e542a151e1
AAA4002911 <zeeshan4002911@gmail.com>
2023-03-03 20:29:34 +0530

    delete and edit task added

--- Todo-List/src/app/app.component.ts
+++ Todo-List/src/app/app.component.ts
@@ -6,15 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-List';
-  
-  tasks:any[] = [];
-  
+  title = 'TODO';
+  tasks: any[] = [];
+  id_count: number = 0;
+  input: string = "";
+  show_save = false;
+  id_temp = "";
+
   addTask(text: string) {
+    if (!text) return;
+    const unique_id: string = 'ID' + this.id_count; 
     this.tasks.push({
-      id : this.tasks.length,
-      name : text
+      id: unique_id,
+      name: text
+    });
+    this.id_count++;
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((current) => current.id !== id);
+  }
+
+  editTask(id: string) {
+    this.tasks.map((current) => {
+      if (current.id === id) {
+        this.input = current.name;
+        this.id_temp = id;
+      }
     });
   }
 
+  saveTask(text: string) {
+    this.tasks.map(current => {
+      if (current.id === this.id_temp) {
+        current.name = text;
+      }
+    })
+    this.id_temp = "";
+    this.show_save = false;
+  }
 }
