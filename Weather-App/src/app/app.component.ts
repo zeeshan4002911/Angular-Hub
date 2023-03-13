4d08f3461c560419d4d67df8318df9ed50697aaf
Md Zeeshan <zeeshan.md@flutura.com>
2023-03-13 11:42:18 +0530

    initial commit

---
+++ Weather-App/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Weather';
+  fetchedData: any = '';
+  fetchedWeather = false;
+  async searchFunc(str: string) {
+    if (str === '') return;
+    console.log(environment);
+    await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${environment.apiKey}&q=${str}`
+    )
+      .then((response: any) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data: any) => (this.fetchedData = data));
+
+    console.log(this.fetchedData);
+  }
+}
---
+++ Weather-App/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    title: 'Weather API',
+    apiKey: '299240f237b94748a8961523230703'
+  };
